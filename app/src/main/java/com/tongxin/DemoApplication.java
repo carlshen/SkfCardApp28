c760acf5edf1807503a8abdaf7e679425c1e8a16
carl shen <xiangfeng.shen@yahoo.com>
2019-11-25 10:37:07 +0800

    1st version for sdk28

---
+++ app/src/main/java/com/tongxin/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.tongxin;
+
+import android.app.Application;
+
+/**
+ * Created by carl on 2019/11/16.
+ */
+public class DemoApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
